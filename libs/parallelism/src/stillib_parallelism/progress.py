1158a5b0144e2902dfb6b6a6c038a0d06672b3b1
Andreas Stillits <andreas.stillits@nbi.ku.dk>
2026-04-24 14:38:52 +0200

    wired a little estimated time addition to progress_callback

--- libs/parallelism/src/stillib_parallelism/progress.py
+++ libs/parallelism/src/stillib_parallelism/progress.py
@@ -10,14 +10,21 @@ def print_progress(update: ProgressUpdate) -> None:
     Live progress update printer to stdout.
     """
     total = "?" if update.total is None else str(update.total)
+    estimated_time_left = None
+    if update.total is not None and update.completed > 0:
+        time_per_task = update.elapsed_time / update.completed
+        estimated_time_left = time_per_task * (update.total - update.completed)
+
     line = (
         f"submitted: {update.submitted},  "
         f"completed: {update.completed},  "
         f"failed: {update.failures},  "
         f"running: {update.running},  "
-        f"total: {total}   "
+        f"total: {total}  "
         f"elapsed: {update.elapsed_time:.2f}s  "
     )
+    if estimated_time_left is not None:
+        line += f"estimated time left: {estimated_time_left:.2f}s"
 
     # clear line, return to start, print new status
     sys.stdout.write("\r\033[2K" + line)
